e544f621025dc83099486a2594086151e88ce2ae
kiruthi <kiruthir2609@gmail.com>
2026-07-27 13:27:56 +0530

    configure variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vomxmkndtvienodtpogo.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbXhta25kdHZpZW5vZHRwb2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MDkzMDIsImV4cCI6MjA5NzE4NTMwMn0.bBnXUdOax_f6eOUszsadnKIZHKm4llzv5WbipEmkgQM";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
